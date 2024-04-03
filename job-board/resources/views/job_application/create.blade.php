2bc30ae16499a48a070b83a32f3e6c81d7a697c6
NjauSamuel <njaus602@gmail.com>
2024-04-03 19:13:01 +0300

    Refactored all the labels and now they are in a label component known as label.blade.php and they are managed from a class called Label.php. After this addition, we reused this newly created component in the 2 create.blade.php and tested them out.

--- job-board/resources/views/job_application/create.blade.php
+++ job-board/resources/views/job_application/create.blade.php
@@ -12,14 +12,14 @@
             @csrf
 
             <div class='mb-4'>
-                <label for="expected_salary" class="mb-2 block font-medium text-slate-900">
+                <x-label for="expected_salary" :required="true">
                     Expected Salary
-                </label>
+                </x-label>
                 <x-text-input type="number" name="expected_salary"/>
             </div>
 
             <div class="mb-4">
-                <label for="cv" class="mb-2 block font-medium text-slate-900">Upload CV</label>
+                <x-label for="cv" :required="true">Upload CV</x-label>
                 <x-text-input type="file" name="cv" />
             </div>
 
