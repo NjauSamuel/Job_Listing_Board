8c308ec8317a4f41073e8758b940a19f2a12336b
NjauSamuel <njaus602@gmail.com>
2024-04-03 23:45:37 +0300

    Began the first steps for creating an employer account. This time, a registerd employee can register to post jobs also.

--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MyJobApplicationController;
+use App\Http\Controllers\EmployerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,9 +35,13 @@ Route::delete('auth', [AuthController::class, 'destroy'])
     ->name('auth.destroy');
 
 Route::middleware('auth')->group(function () {
+
     Route::resource('job.application', JobApplicationController::class)->only(['create', 'store']);
 
     Route::resource('my-job-applications', MyJobApplicationController::class)
         ->only(['index', 'destroy']);
+
+    Route::resource('employer', EmployerController::class)
+        ->only(['create', 'store']);
 
 });
